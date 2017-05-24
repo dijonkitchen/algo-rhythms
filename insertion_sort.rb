1d56c06cc9583f6958b87ef911f3ca36f6601352
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-05-24 11:23:32 -0400

    Remove unnecessary do keyword
    Code style

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,7 +1,7 @@
 def insertion_sort(arr)
   arr.each_with_index do |num, index|
     prev_index = index - 1
-    while prev_index >= 0 && arr[prev_index] > num do
+    while prev_index >= 0 && arr[prev_index] > num
       arr[prev_index + 1] = arr[prev_index]
       prev_index -= 1
     end
