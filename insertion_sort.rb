be96095bd9bafe6ef2ed1b74dc2f978c77378c3f
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-05-24 11:40:16 -0400

    Add Insertion Sort intuition comments

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,3 +1,15 @@
+# Intuition:
+# You're holding a mixed hand of cards and need to order it
+
+# Start comparing cards until you find something out of place
+# Then you go back one card at a time until it's in the right spot
+# Continue until the end
+
+# Good for lists that are almost sorted and
+# off by only a little relative to the size of the list
+# Time complexity would be O(n*k),
+# where k is the margin of error of an element to it's proper position
+
 def insertion_sort(arr)
   arr.each_with_index do |num, index|
     prev_index = index - 1
