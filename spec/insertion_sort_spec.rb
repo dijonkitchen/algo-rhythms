033ead6fcdb13c1fbc5c335df4c6ffccbf9a00fb
Jonathan Chen <dijonkitchen@users.noreply.github.com>
2017-05-24 07:09:50 -0400

    Add spacing in array for style

--- spec/insertion_sort_spec.rb
+++ spec/insertion_sort_spec.rb
@@ -2,7 +2,7 @@ require_relative '../insertion_sort'
 
 RSpec.describe 'Insertion Sort' do
   it 'Sorts arrays' do
-    array = [1,234,12,34,1234,1,234,34]
+    array = [1, 234, 12, 34, 1234, 1, 234, 34]
     expect(insertion_sort(array)).to eq(array.sort)
   end
 end
--- spec/merge_sort_spec.rb
+++ spec/merge_sort_spec.rb
@@ -2,7 +2,7 @@ require_relative '../merge_sort'
 
 RSpec.describe 'Merge Sort' do
   it 'Sorts arrays' do
-    array = [1,234,12,34,1234,1,234,34]
+    array = [1, 234, 12, 34, 1234, 1, 234, 34]
     expect(merge_sort(array)).to eq(array.sort)
   end
 end
